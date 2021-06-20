34fe8b0f29ad32336fb84df371c1aed23ff24b2e
Victor Alarcon <valfonsob98@gmail.com>
2021-06-20 04:16:15 -0400

    fix: correct minor typo in code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-    baseURL: 'http://gateway.marvel.com/v1/public/'
+    baseURL: 'https://gateway.marvel.com/v1/public/'
 });
 
 export default clienteAxios;
